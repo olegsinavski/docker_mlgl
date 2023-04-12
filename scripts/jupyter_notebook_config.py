9c3d37d097b9391a14ca66f06b14525ca92e0644
Oleg Sinavski <sinavski@wayve.ai>
2023-04-12 19:15:06 +0100

    fix_root_dir

--- scripts/jupyter_notebook_config.py
+++ scripts/jupyter_notebook_config.py
@@ -3,7 +3,7 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = ''
 c.NotebookApp.password_required = False
-c.NotebookApp.root_dir = '/example/example'
+c.NotebookApp.root_dir = '/src/'
 
 ## Whether to allow the user to run the notebook as root.
 c.NotebookApp.allow_root = True
